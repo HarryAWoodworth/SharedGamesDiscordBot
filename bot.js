1e4d4966acccaf6fb98ae27745d0aa253f1085ee
LiamBewley <m.j.maeeljavanar@gmail.com>
2021-09-13 19:36:06 -0400

    Add intents

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const config = require('./config.json');
 
 // Initialize Discord Bot
-const bot = new Discord.Client();
+const bot = new Discord.Client({ws: {intents: ['GUILD_MESSAGES', 'GUILD_PRESENCES', 'GUILD_VOICE_STATES']}});
 const distube = new DisTube.default(bot);
 bot.commands = new Discord.Collection();
 const dmOnlyMode = config.dmOnly;
