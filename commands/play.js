9d65fb94bf41867720183093ae6e5819a2b74926
Harry Woodworth <harryawoodworth@gmail.com>
2021-10-15 17:26:04 -0400

    Put the try catch back

--- commands/play.js
+++ commands/play.js
@@ -9,7 +9,11 @@ module.exports = {
     args: true,
     distube: true,
     async execute(message, args, config, bot, db, distube) {
-      console.log("PLaying a song")
-      distube.play(message, args.join(' '));
+      try {
+        distube.play(message, args.join(' '));
+      } catch (error) {
+        console.log("\nERROR WITH PLAY COMMAND:\n");
+        console.error(error);
+      }
     },
 };
