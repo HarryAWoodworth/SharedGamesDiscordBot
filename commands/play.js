a36cb9cf5bedc5f72b239c0a02cb36f37319e6d9
Harry Woodworth <harryawoodworth@gmail.com>
2021-10-14 21:40:46 -0400

    Add try/catch blocks to play and skip commands

--- commands/play.js
+++ commands/play.js
@@ -9,6 +9,11 @@ module.exports = {
     args: true,
     distube: true,
     async execute(message, args, config, bot, db, distube) {
+      try{
         distube.play(message, args.join(' '));
+      } catch (error) {
+        console.log("\nERROR WITH PLAY COMMAND:\n");
+        console.error(error);
+      }
     },
 };
--- commands/skip.js
+++ commands/skip.js
@@ -7,6 +7,11 @@ module.exports = {
     cooldown: 3,
     distube: true,
     async execute(message, args, config, bot, db, distube) {
+      try {
         distube.skip(message);
+      } catch (error) {
+        console.log("\nERROR WITH SKIP COMMAND:\n");
+        console.error(error);
+      }
     },
 };
