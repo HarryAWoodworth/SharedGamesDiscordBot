6bf93faf4fb6ebf4c83839dcfb6b29a09fdd021f
Harry Woodworth <harryawoodworth@gmail.com>
2021-10-14 21:48:04 -0400

    Unbreak play

--- commands/play.js
+++ commands/play.js
@@ -9,11 +9,6 @@ module.exports = {
     args: true,
     distube: true,
     async execute(message, args, config, bot, db, distube) {
-      try{
         distube.play(message, args.join(' '));
-      } catch (error) {
-        console.log("\nERROR WITH PLAY COMMAND:\n");
-        console.error(error);
-      }
     },
 };
